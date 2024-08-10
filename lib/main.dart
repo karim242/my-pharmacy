70568a4196469d9b99926846b05432a2c5a68e4a
Ahmed Slman <ahmedslman151@gmail.com>
2024-08-10 12:11:18 +0300

    setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:my_pharmacy/core/utils/app_router.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:my_pharmacy/firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const Mypharmacy());
 }
-
 
 class Mypharmacy extends StatelessWidget {
   const Mypharmacy({super.key});
@@ -14,31 +18,28 @@ class Mypharmacy extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: const Size(360, 690),
-      minTextAdapt: true,
-      splitScreenMode: true,
-      // Use builder only if you need to use library outside ScreenUtilInit context
-      builder: (_ , child) {
-        return MaterialApp.router(
-
-        routerConfig: AppRouter.router,
-      debugShowCheckedModeBanner: false,
-  locale: const Locale('ar'), // Set default locale to Arabic
-      localizationsDelegates:const [
-       // AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales:const [
-         Locale('en', ''),
-         Locale('ar', ''),
-      ],
-       theme: ThemeData(
-          fontFamily: 'Cairo',
-        ),
-    );
-   }
-    );
+        designSize: const Size(360, 690),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (_, child) {
+          return MaterialApp.router(
+            routerConfig: AppRouter.router,
+            debugShowCheckedModeBanner: false,
+            locale: const Locale('ar'),
+            localizationsDelegates: const [
+              // AppLocalizations.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            supportedLocales: const [
+              Locale('en', ''),
+              Locale('ar', ''),
+            ],
+            theme: ThemeData(
+              fontFamily: 'Cairo',
+            ),
+          );
+        });
   }
 }
